03f8c351bd3a9e004d574ef6f4b68365d62d8d69
AlexZiz <alexzizl@yandex.ru>
2018-03-14 11:36:37 +0300

    add animation for section advantages

--- js/main.js
+++ js/main.js
@@ -1,44 +1,10 @@
-// menu-1===================================================
-var turn_key_ul = $('.turn_key_ul'),
-    timeoutId;
-$('.turn_key').hover(function(){
-    clearTimeout(timeoutId);
-    turn_key_ul.show();
-}, function(){
-    timeoutId = setTimeout($.proxy(turn_key_ul,'hide'), 1000)
-});
-turn_key_ul.mouseenter(function(){
-    clearTimeout(timeoutId);
-}).mouseleave(function(){
-    turn_key_ul.hide();
-});
-
-// menu-2=================================================
-var affordable_ul = $('.affordable_ul'),
-    timeoutId;
-$('.affordable_prices').hover(function(){
-    clearTimeout(timeoutId);
-    affordable_ul.show();
-}, function(){
-    timeoutId = setTimeout($.proxy(affordable_ul,'hide'), 1000)
-});
-affordable_ul.mouseenter(function(){
-    clearTimeout(timeoutId);
-}).mouseleave(function(){
-    affordable_ul.hide();
-});
-
-// menu-3====================================================
-var quality_ul = $('.quality_ul'),
-    timeoutId;
-$('.high_quality').hover(function(){
-    clearTimeout(timeoutId);
-    quality_ul.show();
-}, function(){
-    timeoutId = setTimeout($.proxy(quality_ul,'hide'), 1000)
-});
-quality_ul.mouseenter(function(){
-    clearTimeout(timeoutId);
-}).mouseleave(function(){
-    quality_ul.hide();
+// плавное появление блоков приимуществ
+$(window).ready(function(){
+  /* немного другой эффект появления, и задержка в 2 секунды */
+  setTimeout ("$('#block1').fadeIn();",1000);
+  setTimeout ("$('#block2').fadeIn();",1500);
+  setTimeout ("$('#block3').fadeIn();",2000);
+  setTimeout ("$('#block4').fadeIn();",2500);
+  setTimeout ("$('#block5').fadeIn();",3000);
+  setTimeout ("$('#block6').fadeIn();",3500);
 });
